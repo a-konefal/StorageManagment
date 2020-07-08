2fdab110d0942ac279fd9e5dd615713a67f00376
a-konefal <58752224+a-konefal@users.noreply.github.com>
2020-07-08 20:55:35 +0200

    Unit Test

---
+++ StorageTest/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StorageMagazine;
+
+namespace StorageTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Quantity_should_return_true()
+        {
+            // Arrange
+            
+            bool expected = true;
+            SharedSqlCommand sharedsqlcommand = new SharedSqlCommand();
+
+            // Act
+            
+            bool actual =  sharedsqlcommand.QuantityValidation("10");
+
+            // Assert
+            
+            Assert.AreEqual(expected, actual,  "Mimo poprawnej wartości zwraca false");
+        }
+        [TestMethod]
+        public void Quantity_should_return_false()
+        {
+            // Arrange
+
+            bool expected = false;
+            SharedSqlCommand sharedsqlcommand = new SharedSqlCommand();
+
+            // Act
+
+            bool actual = sharedsqlcommand.QuantityValidation("test");
+
+            // Assert
+
+            Assert.AreEqual(expected, actual, "Mimo błędnej wartości zwraca true");
+        }
+        [TestMethod]
+        public void Date_Time_should_return_true()
+        {
+            // Arrange
+
+            bool expected = true;
+            SharedSqlCommand sharedsqlcommand = new SharedSqlCommand();
+
+            // Act
+
+            bool actual = sharedsqlcommand.DateTimeValidation("20-09-2019");
+
+            // Assert
+
+            Assert.AreEqual(expected, actual, "Mimo poprawnej wartości zwraca false");
+        }
+        [TestMethod]
+        public void Date_Time_should_return_false()
+        {
+            // Arrange
+
+            bool expected = false;
+            SharedSqlCommand sharedsqlcommand = new SharedSqlCommand();
+
+            // Act
+
+            bool actual = sharedsqlcommand.DateTimeValidation("test");
+
+            // Assert
+
+            Assert.AreEqual(expected, actual, "Mimo błędnej wartości zwraca true");
+        }
+    }
+}
